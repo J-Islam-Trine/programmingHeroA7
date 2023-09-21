c77a7842e32fa7041329d84334018393f4aa899d
Jahirul Islam <j.islam.trine@gmail.com>
2023-09-21 22:39:08 +0600

    Created main section.

---
+++ src/components/features/header.jsx
@@ -0,0 +1,13 @@
+// import react
+import React from "react";
+
+
+//declare funtion
+const Header = () => {
+    return(
+
+        <h1 className="font-Inter text-4xl text-center mt-12 mb-8">Course Registration</h1>
+    )
+}
+
+export default Header
