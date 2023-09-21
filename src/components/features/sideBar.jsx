55d015940b40718890716cdcef000d3741388691
Jahirul Islam <j.islam.trine@gmail.com>
2023-09-22 01:08:37 +0600

    Added price and remaining credit calc function.

--- src/components/features/sideBar.jsx
+++ src/components/features/sideBar.jsx
@@ -3,12 +3,34 @@ import React from "react";
 
 
 //declare funtion
-const SideBar = () => {
+const SideBar = ({credit, courseList, price}) => {
     return(
        <div className=" w-80 bg-white p-6">
-            <div>
-                <p>Credit Hour Remaining 7 hr</p>
+            <div className="pb-4">
+                <p className=" text-lg font-bold leading-7 text-newBlue">Credit Hour Remaining {20-credit} hr</p>
             </div>
+            <hr></hr>
+            <div className="pt-4 pb-6">
+                    <h3 className=" text-xl font-bold">Course Name</h3>
+
+                    <ul>
+                        {
+                             courseList.map((courseItem, index) => 
+                             <p className=" text-base font-normal leading-7" key={index}>{index+1} {courseItem}</p>
+                          )
+                           
+                        }
+                    </ul>
+            </div>
+            <hr />
+            <div className="my-4">
+                <p className=" font-semibold">Total Credit Hour: {credit}</p>
+            </div>
+            <hr />
+            <div className="my-4">
+                <p className=" font-semibold">Total Price: {price} USD</p>
+            </div>
+
        </div>
     )
 }
